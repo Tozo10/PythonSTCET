e77f8a830661ab72e62f150b45432d8092b9eb06
Tozo10 <sayantanchatterjee00@gmail.com>
2023-08-22 15:59:55 +0530

    second try

---
+++ 22.08.23/sum.py
@@ -0,0 +1,5 @@
+n = int(input("Enter the value of N : "))
+s = 0
+for i in range (0,n):
+    s = s + i
+print("Sum : ",s)
---
+++ 22.08.23/test.py
@@ -0,0 +1,8 @@
+s = "hello sir!!"
+count = 0
+print("***** Itereate ****")
+for elem in s:
+    if(elem == 'l'):
+       count = count + 1
+print (count)
+    
